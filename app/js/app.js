bf9b6528ad11ed658264cf5d8bae31752fbe3a8f
Anton <lakirienkol@gmail.com>
2020-12-16 14:31:59 +0200

    com

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,32 @@
 // import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
+import CardsService from "./services/cards.service";
 
-document.addEventListener('DOMContentLoaded', () => {
+const cardsService = new CardsService();
+const loginBtn = document.getElementById("loginBtn");
+const createNewMeet = document.getElementById("createNewMeet");
 
-	// Custom JS
+document.addEventListener("DOMContentLoaded", () => {
+  // Custom JS
+  const root = document.getElementById("root");
 
-})
+  const writeMarkup = (cards) => {
+    const ul = document.createElement("ul");
+    for (const card of cards) {
+      ul.innerHTML += `<li>${card.content.name}</li>`;
+    }
+    root.appendChild(ul);
+  };
+
+  const getAllCards = async () => {
+    const data = await cardsService.fetchCards();
+    writeMarkup(data);
+  };
+
+  getAllCards();
+});
+
+loginBtn.addEventListener("click", () => {
+  loginBtn.style = "display:none";
+  createNewMeet.style = "display:block";
+  loginBtn.preventDefault()
+});
